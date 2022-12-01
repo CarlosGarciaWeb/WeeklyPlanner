9b7991841072b76f4dbe29d42b88f9025af42ab2
Carlos García <carlos.grh@outlook.com>
2022-12-01 14:51:50 -0500

    create functionality that creates date object when a specific date gets accessed if it doesn't exist already. This date date object has a task manager object that can contain a dict of tasks, their id and their length and will also contain the amount of available hours for that day and amount of hours dedicated to tasks, also created functionality that removes hours from available hours on date object according to the specific date and task lengths

--- app.py
+++ app.py
@@ -1,78 +1,31 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, current_app
 from pymongo import MongoClient
 from datetime import datetime as dtt, date as dt, time as t, timedelta
 import time
 from collections import defaultdict
 import uuid
+import os
 
 #TODO: Ultimate goal is to learn Javascript in order to create a countdown timer in screen and show current progress
 #TODO: Render form and dates to organize planner
 
 
-print(dtt.today(), dt.today, f"\n\n")
-
 app = Flask(__name__)
 
-list_hours = [hour for hour in range(1,9)]
+mongo_address = os.environ.get("MONGODB_ADDRESS")
 
+client = MongoClient(mongo_address)
+
+app.db = client.flask
 
 def returnToday():
     today = dtt.today()
     return dtt(today.year, today.month, today.day)
 
-taskCounter = defaultdict(t)
+possible_hours = [hour for hour in range(1,9)]
 
-taskManager = defaultdict(dict)
+# {"_id": uuid, date: selected_date, taskManager: {uuid: {"_id": uuid, "name": taskName,"length": length}}, hoursTaken: [], hoursAvailable: []}}
 
-dayManager = defaultdict(list)
-
-appRunning = False
-
-# while appRunning:
-#     today_date = returnToday()
-#     hoursToday = sum(dayManager[today_date])
-#     hoursToRemove = []
-#     if hoursToday > 0:
-#         print(hoursToday>0)
-#         for item in list_hours:
-#             if item+hoursToday > 8:
-#                 print(item+hoursToday)
-#                 hoursToRemove.append(item)
-#     for item in hoursToRemove:
-#         list_hours.remove(item)
-#     if hoursToday < 8:
-#         assignTask = input(f"Assign a task for Today, {dt.today().strftime('%a, %d')}: ")
-#         for item in list_hours:
-#             print(item)
-#         selectedHour = int(input(f"\nChoose an hour from the above: "))
-#         taskCounter[assignTask] = selectedHour
-#         taskManager[today_date] = taskCounter
-#         dayManager[today_date].append(selectedHour)
-#     else:
-#         appRunning = False
-
-def calculatePossibleHours(date: dtt):
-    global dayManager, list_hours
-    hoursToRemove = []
-    hoursToday = sum(dayManager[date])
-    if hoursToday > 0:
-        for hour in list_hours:
-            if hour+hoursToday > 8:
-                hoursToRemove.append(hour)
-    for hourR in hoursToRemove:
-        list_hours.remove(hourR)
-    return hoursToday
-
-# {dateId: [1, 2, 3, 5]}
-
-# {task: [1, False]}
-
-# {"_id": id, date: {task:1,task2:2}}
-
-
-
-
-test_list = []
 
 
 @app.context_processor
@@ -83,7 +36,8 @@ def calc_dateRange():
         return dates_list
     return {"date_range": selectDates}
 
-
+def create_task_date_syntax(date: dtt, hours: list):
+    return {"date": date, "taskManager": {}, "hoursTaken": [], "hoursAvailable": hours}
 
 @app.route("/")
 def home():
@@ -93,43 +47,96 @@ def home():
     else:
         selected_date = returnToday()
     todays_day = returnToday()
-    hoursAvailable = calculatePossibleHours()
+    current_task_date_object = app.db.taskManager.find_one({"date": selected_date})
+    if not current_task_date_object:
+        app.db.taskManager.insert_one(create_task_date_syntax(date=selected_date, hours=possible_hours))
+        current_task_date_object = app.db.taskManager.find_one({"date": selected_date})
+    current_task_manager = current_task_date_object["taskManager"]
+    if current_task_manager:
+        task_lists = [(task["name"],task["length"]) for task in current_task_manager.values()]
+    else:
+        task_lists = []
+
+
+
+    if selected_date == todays_day:
+        tomorrows_date = todays_day + timedelta(days=1)
+        preview_task_object = app.db.taskManager.find_one({"date": tomorrows_date})
+        if not preview_task_object:
+            app.db.taskManager.insert_one(create_task_date_syntax(date=selected_date, hours=possible_hours))
+    else:
+        preview_task_object = app.db.taskManager.find_one({"date": todays_day})
+    preview_task_manager = preview_task_object["taskManager"]
+    if preview_task_manager:
+        preview_tasks = [(task["name"], task["length"]) for task in preview_task_manager.values()]
+    else:
+        preview_tasks = []
+
+
+    
     return render_template(
         'home.html', 
         selected_date=selected_date, 
         title="WeeklyPlanner | Home", 
-        test_list=test_list,
+        task_list=task_lists,
         todays_day=todays_day,
-        hoursAvailable=hoursAvailable
+        preview=preview_tasks
     )
 
 
     
+# myquery = { "address": "Valley 345" }
+# newvalues = { "$set": { "address": "Canyon 123" } }
+
+# mycol.update_one(myquery, newvalues)
 
 
 
 @app.route("/add-task", methods=["GET", "POST"])
 def addTask():
-    global taskCounter, taskManager, dayManager
     date_string = request.args.get("date")
-    hoursAvailable = request.args.get("hours")
     if date_string:
         selectedDate = dtt.fromisoformat(date_string)
     else:
         selectedDate = returnToday()
+    query_date = {"date": selectedDate}
+    date_task_object = app.db.taskManager.find_one(query_date)
+    if not date_task_object:
+        app.db.taskManager.insert_one(create_task_date_syntax(date=selectedDate, hours=possible_hours))
+        date_task_object = app.db.taskManager.find_one(query_date)
+    hours_list_available = date_task_object["hoursAvailable"]
     if request.method == "POST" and "date" in request.form:
         response = request.form.get("date")
         date_formated = dtt.fromisoformat(response)
-        return redirect(url_for("addTask", date=date_formated, list_hours=list_hours))
-    if hoursAvailable < 8:
-        if request.method == "POST" and "task" in request.form:
-            response = (request.form.get("task"), request.form.get("hour"))
-            taskCounter[response[0]] = response[1]
-            taskManager[selectedDate] = taskCounter
-            dayManager[selectedDate].append(response[1])
-            hoursAvailable = calculatePossibleHours()
-            return redirect(url_for("addTask", selectedDate=selectedDate, list_hours=list_hours))
-    return render_template('add.html', selectedDate=selectedDate, list_hours=list_hours)
+        return redirect(url_for("addTask", date=date_formated))
+    if request.method == "POST" and "task" in request.form:
+        response = (request.form.get("task"), int(request.form.get("hour")))
+        idTask = uuid.uuid4().hex
+        new_task = {"_id": idTask ,"name": response[0], "length": response[1]}
+        current_task_manager = date_task_object["taskManager"]
+        current_task_manager[idTask] = new_task
+
+        current_hours_taken = date_task_object["hoursTaken"]
+        current_hours_taken.append(response[1])
+
+        current_hours_available = date_task_object["hoursAvailable"]
+
+        hours_to_remove = []
+
+        for hour_available in current_hours_available:
+            if sum(current_hours_taken) + int(hour_available) > 8:
+                hours_to_remove.append(hour_available)
+
+
+        for hour_remove in hours_to_remove:
+            current_hours_available.remove(hour_remove)
+
+
+        updatedValues = {"$set": {"taskManager": current_task_manager, "hoursTaken": current_hours_taken, "hoursAvailable": current_hours_available}}
+        app.db.taskManager.update_one(query_date, updatedValues) 
+
+        return redirect(url_for("addTask", selectedDate=selectedDate))
+    return render_template('add.html', selectedDate=selectedDate, list_hours=hours_list_available)
 
 
 
